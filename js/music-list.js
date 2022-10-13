e597d074972446f0a990e71ea009fb217982df8a
Lohitaksh Nandan <94240494+lohitakshnandan@users.noreply.github.com>
2022-10-13 09:02:23 +0530

    Update music-list.js

--- js/music-list.js
+++ js/music-list.js
@@ -11,40 +11,40 @@
 
 let allMusic = [
   {
-    name: "Harley Bird - Home",
-    artist: "Jordan Schor",
-    img: "music-1",
-    src: "music-1"
+    name: "English Songs Jukebox",
+    artist: "Lohitaksh",
+    img: "player1",
+    src: "song1"
   },
   {
-    name: "Ikson Anywhere – Ikson",
-    artist: "Audio Library",
-    img: "music-2",
-    src: "music-2"
+    name: "Mind Relax Mashup",
+    artist: "Lohitaksh",
+    img: "player2",
+    src: "song2"
   },
   {
-    name: "Beauz & Jvna - Crazy",
-    artist: "Beauz & Jvna",
-    img: "music-3",
-    src: "music-3"
+    name: "Alone Sad Jukebox",
+    artist: "Lohitaksh",
+    img: "player3",
+    src: "song3"
   },
   {
-    name: "Hardwind - Want Me",
-    artist: "Mike Archangelo",
-    img: "music-4",
-    src: "music-4"
+    name: "Best of Arijit Singh",
+    artist: "Arijit Singh",
+    img: "player4",
+    src: "song4"
   },
   {
-    name: "Jim - Sun Goes Down",
-    artist: "Jim Yosef x Roy",
-    img: "music-5",
-    src: "music-5"
+    name: "Punjabi Mashup",
+    artist: "Lohitaksh",
+    img: "player5",
+    src: "song5"
   },
   {
-    name: "Lost Sky - Vision NCS",
-    artist: "NCS Release",
-    img: "music-6",
-    src: "music-6"
+    name: "Party Mashup",
+    artist: "Lohitaksh",
+    img: "player6",
+    src: "song6"
   },
   // like this paste it and remember to give comma after ending of this bracket }
   // {
